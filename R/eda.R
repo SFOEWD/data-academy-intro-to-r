c35083a9b3796e451fa4d159c9fe4d1e84acf44b
daranzolin <daranzolin@gmail.com>
2025-02-12 19:33:20 -0800

    refactor

--- R/eda.R
+++ R/eda.R
@@ -5,7 +5,8 @@ library(stringr)
 library(tidyverse)
 
 # Import data
-incidents_raw <- read.socrata("https://data.sfgov.org/resource/wg3w-h783.csv")
+# incidents_raw <- read.socrata("https://data.sfgov.org/resource/wg3w-h783.csv")
+incidents_raw <- read_rds("data/incidents_raw.rds")
 glimpse(incidents_raw)
 
 # Clean data
@@ -18,9 +19,27 @@ incidents <- incidents_raw %>%
     # 'True' TRUE/FALSE
     filed_online = filed_online == "true",
     # Empty strings to NA
-    incident_category = if_else(incident_category == "", NA, incident_category)
+    incident_category = if_else(incident_category == "", NA, incident_category),
+    # Harmonize labels
+    incident_category = case_match(
+      incident_category,
+      "Weapons Offence" ~ "Weapons Offense",
+      "Motor Vehicle Theft?" ~ "Motor Vehicle Theft",
+      .default = incident_category
+      )
     ) %>%
   select(-c(supervisor_district_2012:point))
+
+# The utility of true logicals
+incidents %>% filter(filed_online)
+
+incidents %>%
+  group_by(incident_year) %>%
+  summarize(perc_filed_online = mean(filed_online))
+
+incidents %>%
+  mutate(is_weekend = wday(incident_date) %in% 6:7) %>%
+  summarize(perc_weekend = mean(is_weekend))
 
 # Count stuff!
 incidents %>% count(analysis_neighborhood, sort = TRUE)
@@ -60,50 +79,34 @@ incidents_on_market %>%
 
 # Looks like most were 'Larceny Theft' during the last weekend in June
 # Question: how does incidents of Larceny Theft compare to the weekends before and after?
-is_second_to_last_weekend_of_june <- function(x) {
-  is_june <- month(x) == 6
+bin_weekends <- function(x) {
+  month <- month(x)
+  mday <- mday(x)
   is_weekend <- wday(x) %in% c(7, 1)
   last_day_of_month <- ceiling_date(x, "month") - days(1)
-  is_in_last_14_days_of_month <- between(mday(last_day_of_month) - mday(x), 7, 14)
-  return(is_june & is_weekend & is_in_last_14_days_of_month)
-}
-
-is_last_weekend_of_june <- function(x) {
-  is_june <- month(x) == 6
-  is_weekend <- wday(x) %in% c(7, 1)
-  last_day_of_month <- ceiling_date(x, "month") - days(1)
-  is_in_last_7_days_of_month <- mday(last_day_of_month) - mday(x) < 7
-  return(is_june & is_weekend & is_in_last_7_days_of_month)
-}
-
-is_first_weekend_of_july <- function(x) {
-  is_july <- month(x) == 7
-  is_weekend <- wday(x) %in% c(7, 1)
-  first_day_of_month <- floor_date(x, "month")
-  is_in_first_7_days_of_month <- mday(x) < 7
-  return(is_july & is_weekend & is_in_first_7_days_of_month)
+  days_from_last_mday <- mday(last_day_of_month) - mday
+  out <- case_when(
+    month == 6 & is_weekend & between(days_from_last_mday, 7, 14) ~ "Second to last weekend in June",
+    month == 6 & is_weekend & between(days_from_last_mday, 0, 6) ~ "Last weekend in June",
+    month == 7 & is_weekend & mday < 7 ~ "First weekend in July",
+    .default = "Outside scope"
+  )
+  return(out)
 }
 
 plot_data <- incidents_on_market %>%
-  mutate(
-    weekend = case_when(
-      is_second_to_last_weekend_of_june(incident_date) ~ "Second to last weekend in June",
-      is_last_weekend_of_june(incident_date) ~ "Last weekend in June",
-      is_first_weekend_of_july(incident_date) ~ "First weekend in July",
-      .default = "Outside scope"
-      )
-    ) %>%
+  mutate(weekend = bin_weekends(incident_date)) %>%
+  filter(
+    weekend != "Outside scope",
+    incident_category == "Larceny Theft"
+  ) %>%
   # Factor weekend variable 'in order'
   mutate(
     weekend = factor(
       weekend,
       levels = c("Second to last weekend in June", "Last weekend in June", "First weekend in July")
     )
-  ) %>%
-  filter(
-    weekend != "Outside scope",
-    incident_category == "Larceny Theft"
-    )
+  )
 
 plot_data %>%
   count(weekend) %>%
