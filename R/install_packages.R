b61bf0c22f03adeb4f8b2423b054d94c2828ee38
daranzolin <daranzolin@gmail.com>
2025-04-29 11:13:47 -0700

    install from r-universe

--- R/install_packages.R
+++ R/install_packages.R
@@ -7,3 +7,6 @@ install.packages(
     "writexl"
   )
 )
+
+# UPDATE 2025/04/29 - RSocrata was removed from CRAN. It should reappear shortly, but in the meantime, you can install the package via:
+install.packages("RSocrata", repos = c("https://chicago.r-universe.dev", "https://cloud.r-project.org"))
