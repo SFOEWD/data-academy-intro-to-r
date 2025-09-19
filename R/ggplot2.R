95e68a9e756c6159e008e927cde2a7c819bfdcc7
daranzolin <daranzolin@gmail.com>
2025-09-19 12:51:24 -0700

    gganimate

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -226,6 +226,20 @@ ggplot(d) +
   geom_line(aes(idx, value, colour = type)) +
   gghighlight(max(value) > 20)
 
+library(gganimate)
+library(gapminder)
+
+ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, color = country)) +
+  geom_point(alpha = 0.7, show.legend = FALSE) +
+  scale_colour_manual(values = country_colors) +
+  scale_size(range = c(2, 12)) +
+  scale_x_log10() +
+  facet_wrap(~continent) +
+  # Here comes the gganimate specific bits
+  labs(title = 'Year: {frame_time}', x = 'GDP per capita', y = 'life expectancy') +
+  transition_time(year) +
+  ease_aes('linear')
+
 library(ggrepel)
 set.seed(42)
 
