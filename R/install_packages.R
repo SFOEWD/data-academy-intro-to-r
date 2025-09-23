eb90515e7a16b9ac62d19ed01b11cdb58533b5b0
daranzolin <daranzolin@gmail.com>
2025-09-23 10:58:42 -0700

    Update install_packages.R

--- R/install_packages.R
+++ R/install_packages.R
@@ -1,12 +1,11 @@
-install.packages(
+# install.packages() is the default function for installing packages
+install.packages("pak")
+# pak is a package that makes installing packages much faster, safer, and convenient
+pak::pkg_install(
   c(
     "tidyverse",
     "gt",
-    "RSocrata",
-    "clipr",
-    "writexl"
+    "writexl",
+    "Chicago/RSocrata"
+    )
   )
-)
-
-# UPDATE 2025/04/29 - RSocrata was removed from CRAN. It should reappear shortly, but in the meantime, you can install the package via:
-install.packages("RSocrata", repos = c("https://chicago.r-universe.dev", "https://cloud.r-project.org"))
